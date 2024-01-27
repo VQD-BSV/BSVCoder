07380dd815b87f5aea0f3f073c0030760b505c5f
Vũ Quang Đại (Bevis Vũ) <127699283+VQD-BSV@users.noreply.github.com>
2024-01-27 17:11:40 +0700

    Add files via upload

---
+++ [PyQt6 & Qt Designer]/#7/BSV_Coder.py
@@ -0,0 +1,34 @@
+import sys, time
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from Sc import Ui_MainWindow
+
+class MainWindow:
+    def __init__(self):
+        self.main_win = QMainWindow()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self.main_win)
+
+        self.uic.nut.clicked.connect(self.test)
+
+    def test(self):
+
+        giatri = self.uic.verticalSlider.value()
+        print(f"Chay den: {giatri}")
+        for i in range(1, giatri+1):
+
+            time.sleep(0.2)
+            self.uic.progressBar.setValue(i)
+
+    # Thông báo
+    def Message(self, mess): 
+        QMessageBox(windowTitle="Information",text=mess).exec()
+
+    def show(self):
+        # command to run
+        self.main_win.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
